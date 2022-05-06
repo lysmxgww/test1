26e8851ab9bb9f478d898b22ea3783c32640f747
lysmxgww <lysmx@sohu.com>
2022-05-06 20:50:33 +0800

    创建项目结构

---
+++ start/src/main/java/com/neuralgalaxy/ngiqdeviceserver/NgiqDeviceServerApplication.java
@@ -0,0 +1,13 @@
+package com.neuralgalaxy.ngiqdeviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgiqDeviceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgiqDeviceServerApplication.class, args);
+    }
+
+}
---
+++ start/src/test/java/com/neuralgalaxy/ngiqdeviceserver/NgiqDeviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuralgalaxy.ngiqdeviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgiqDeviceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
